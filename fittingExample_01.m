e4d56b754dad3253f5e255dc53e0ab20d3b476cb
Ekin Tünçok <60780594+ekintuncok@users.noreply.github.com>
2021-04-05 08:32:32 -0400

    Update fittingExample_01.m

--- fittingExample_01.m
+++ fittingExample_01.m
@@ -1,4 +1,4 @@
-addpath('/Users/ekintuncok/Desktop/2021_First Year Project/pRF_AF Model Fit');
+addpath('/Users/et2160/Desktop/Attention_pRF-main');
 mx = @(x) x / max(x(:)); %Because of the small values obtained when AF and RF are multiplied
 % Read in some stimulus apertures
 load('RETBARsmall.mat', 'stim');
@@ -17,13 +17,14 @@ h = t .* exp(-t/5);
 h = h / sum(h); % normalize to sum of 1
 
 % Define the stimulus driven RF parameters
-x = [-4,0,5]; y = [3, 4, -3]; sigma = [1, 2, 3]; % in degrees of visual field
+x = linspace(-10,10,10); y = linspace(-10,10,10); sigma = linspace(1,5,10); % in degrees of visual field
 % Define the attention field parameters
-AF_x = 10; AF_y = 0; AF_sigma = 2;
+AF_x = 12; AF_y = 0; AF_sigma = 2;
 
 % Create pink noise
 % whitenoise = randn(1,300);
 % noise = cumsum(whitenoise);
+% minNoise = min(noise); %not sure how else to fix the scaling problem here. In the following line as well.
 
 % Simulate (one tiny) dataset:
 [~,AFSimVec] = createGauissanFields(X,Y,AF_x,AF_y,AF_sigma);
@@ -43,14 +44,13 @@ data=boldMeasured;
 % 1) stim driven pRF position [x], [y];
 % 2) stim driven pRF size [sigma]
 % 3) attention field size [AF_sigma]
-
 x0 = [0 , 0, 1]; % Define the initial values
-A = [1,0,0;0,1,0]; % Up to 10deg visual field eccentricity & polar angle:
-b = [10;10];
-%lb = [-10, -10, 0]; % Lower bound
+% A = [1,0,0;0,1,0]; % Up to 10deg visual field eccentricity & polar angle:
+% b = [10;10];
+lb = [-10, -10, 0]; % Lower bound
+ub = [10, 10, 5]; % Lower bound
 
 for ii = 1:length(x)
 fun = @(p) (RSSR(p,stim,data(ii,:)));
-fitParams(ii,:) = fmincon(fun,x0,A,b)
+fitParams(ii,:) = fmincon(fun,x0,[],[],[],[],lb,ub);
 end
-
