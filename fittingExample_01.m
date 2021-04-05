3833e1dcac4bb6b2ebf3ccb7ef629045f7aa8f90
Ekin Tünçok <60780594+ekintuncok@users.noreply.github.com>
2021-04-05 09:26:56 -0400

    Update fittingExample_01.m

--- fittingExample_01.m
+++ fittingExample_01.m
@@ -19,7 +19,7 @@ h = h / sum(h); % normalize to sum of 1
 % Define the stimulus driven RF parameters
 x = linspace(-10,10,10); y = linspace(-10,10,10); sigma = linspace(1,5,10); % in degrees of visual field
 % Define the attention field parameters
-AF_x = 12; AF_y = 0; AF_sigma = 2;
+AF_x = 12; AF_y = 0; AF_sigma = 2; %adjusted the AF eccentricity so that it falls outside of the mapped eccentricity
 
 % Create pink noise
 % whitenoise = randn(1,300);
@@ -28,6 +28,9 @@ AF_x = 12; AF_y = 0; AF_sigma = 2;
 
 % Simulate (one tiny) dataset:
 [~,AFSimVec] = createGauissanFields(X,Y,AF_x,AF_y,AF_sigma);
+
+boldMeasured1 = zeros(length(x),2*length(h)-1);
+boldMeasured = zeros(length(x),timepoints);
 
 for ii = 1:length(x)
 [~,RFSimVec] = createGauissanFields(X,Y,x(ii),y(ii),sigma(ii));
@@ -50,7 +53,31 @@ x0 = [0 , 0, 1]; % Define the initial values
 lb = [-10, -10, 0]; % Lower bound
 ub = [10, 10, 5]; % Lower bound
 
+fitParams= zeros(length(x),3);
+
 for ii = 1:length(x)
 fun = @(p) (RSSR(p,stim,data(ii,:)));
 fitParams(ii,:) = fmincon(fun,x0,[],[],[],[],lb,ub);
 end
+
+%% PLOTS
+% See how the parent pRFs change for different stimulus driven pRFs when
+% attenional field is kept constant:
+
+parentpRFs = cell(1,length(x));
+stimpRFs = cell(1,length(x));
+for ii = 1:length(x)
+[parentpRFs{ii},~] = createGauissanFields(X,Y,fitParams(ii,1),fitParams(ii,2),fitParams(ii,3));
+[stimpRFs{ii},~] = createGauissanFields(X,Y,x(ii),y(ii),sigma(ii));
+end
+
+for ii = 1:length(x)
+    figure (1);
+subplot(10,2,2*ii-1)
+imagesc(X(:), Y(:), stimpRFs{ii}); axis image xy; 
+title('Stim-driven pRF')
+subplot(10,2,2*ii)
+imagesc(X(:), Y(:), parentpRFs{ii}); axis image xy; 
+title('Parent pRF (Stim-driven pRF * Attentional field)')
+end
+set(gcf, 'Position',[0, 1000,1000,1000])
