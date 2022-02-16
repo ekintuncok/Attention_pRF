6d3e8253049b299d24ffa48ad7a9f7cd10464076
ekintuncok <tuncokekin@gmail.com>
2022-02-15 22:29:16 -0500

    small changes

--- NMA_simulate.m
+++ NMA_simulate.m
@@ -1,23 +1,25 @@
-%% Space (1D)
+%% Define the params
 npoints = 1001;
-mxecc   = 10;
-x = linspace(-mxecc,mxecc,npoints)'; % degrees
+mxecc   = 10; % degrees
+x       = linspace(-mxecc,mxecc,npoints)'; 
+sd      = 2*[1 1.5]; % size of RF
+voxsd   = 2; % size of voxel (extent of summation)
+attgain = 2; % gain factor of attention field
+attx0   = 0; % location of attention field
+attsd   = 2; % size of AF
+sigma   = .01; % sigma factor of normalization
+
 %% Neural RFs
-sd = 2*[1 1.5];
 RF = exp(-1/2*(x/sd(1)).^2)';
 RF = RF / sum(RF);
 RFsupp = exp(-1/2*(x/(sd(2))).^2)';
 RFsupp = RFsupp / sum(RFsupp);
 %% Voxel summation (across neurons)
-voxsd = 2;
 voxrf = exp(-1/2*(x/voxsd).^2);
 voxrf = voxrf / sum(voxrf);
 %% Stimuli
 stim = eye(npoints);
 %% Attention field
-attgain = 2;
-attx0   = 2;
-attsd   = 2;
 attfield = exp(-1/2*((x-attx0)/attsd).^2);
 attfield = attgain*attfield  + 1;
 %% Stimulus and Suppressive Drive
@@ -25,10 +27,10 @@ stimdrive = conv2(stim, RF, 'same');
 numerator = stimdrive .* (attfield * ones(1,npoints));
 suppdrive = conv2(numerator, RFsupp, 'same');
 %% population response
-sigma = .01;
 popresp = numerator ./ (suppdrive + sigma);
 voxresp = conv2(popresp, voxrf, 'same');
-%% check
+
+%% visualize
 fH = figure(1); clf;
 set(fH, 'Color', 'w');
 lw = 3;
@@ -69,6 +71,7 @@ for ii = 1:length(locs)
     [~, stimidx(ii)] = min(abs(x-(attx0+k)));
 end
 plot(x, popresp(:, stimidx), 'LineWidth', lw);
+ylim([0,1])
 title('Population response');
 set(gca, 'XTick', x(stimidx),  'XGrid', 'on', 'GridAlpha', 1)
 plotOptions(gca, fs);
@@ -77,6 +80,7 @@ hold on
 plot(x(respidx)*[1 1], yl, 'r--')
 subplot(3,3,8);
 plot(x, popresp(stimidx,:), 'LineWidth', lw)
+ylim([0,1])
 title('Neural responses');
 plotOptions(gca, fs);
 hold on; set(gca, 'XTick', x(stimidx),  'XGrid', 'on', 'GridAlpha', 1)
@@ -84,11 +88,15 @@ hold on; set(gca, 'XTick', x(stimidx),  'XGrid', 'on', 'GridAlpha', 1)
 plot(x(respidx)*[1 1], yl, 'r--')
 subplot(3,3,9);
 plot(x, voxresp(stimidx,:), 'LineWidth', lw)
+ylim([0,1])
 title('Voxel responses');
 plotOptions(gca, fs);
 hold on; set(gca, 'XTick', x(stimidx),  'XGrid', 'on', 'GridAlpha', 1)
 [~, respidx] = max(voxresp(stimidx, :),[],2); yl = get(gca, 'YLim');
 plot(x(respidx)*[1 1], yl, 'r--')
+set(gcf,'Position',[0 0 1600 1600])
+
+
 function plotOptions(ax, fs)
 set(ax, 'FontSize', fs)
 end
