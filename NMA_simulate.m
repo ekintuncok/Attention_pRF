b1185df33d6af9c9c10928479388fad43d29bd33
ekintuncok <tuncokekin@gmail.com>
2022-03-01 00:03:47 -0500

    define some functions

--- NMA_simulate.m
+++ NMA_simulate.m
@@ -1,24 +1,31 @@
-%% Define the params
-npoints = 1001;
-mxecc   = 10; % degrees
-x       = linspace(-mxecc,mxecc,npoints)'; 
-sd      = 2*[1 1.5]; % size of RF
-voxsd   = 2; % size of voxel (extent of summation)
-attgain = 2; % gain factor of attention field
-attx0   = 0; % location of attention field
-attsd   = 2; % size of AF
-sigma   = .01; % sigma factor of normalization
+function [x, popresp, summation] = NMA_simulate(params)
 
+npoints     = params(2);
+mxecc       = params(3);
+RFsd        = params(4);
+attgain     = params(5);
+attx0       = params(6);
+attsd       = params(7);
+summationsd = params(8);
+sigma       = params(9);
+visualize   = params(10);
+
+%% Space (1D)
+x = linspace(-mxecc,mxecc,npoints)'; % degrees
 %% Neural RFs
-RF = exp(-1/2*(x/sd(1)).^2)';
+RF = exp(-1/2*(x/RFsd).^2);
 RF = RF / sum(RF);
-RFsupp = exp(-1/2*(x/(sd(2))).^2)';
+RFsupp = exp(-1/2*(x/(RFsd*3)).^2);
 RFsupp = RFsupp / sum(RFsupp);
 %% Voxel summation (across neurons)
-voxrf = exp(-1/2*(x/voxsd).^2);
-voxrf = voxrf / sum(voxrf);
+RFsumm = exp(-1/2*(x/summationsd).^2);
+RFsumm = RFsumm / sum(RFsumm);
 %% Stimuli
-stim = eye(npoints);
+stim = zeros(npoints,npoints);
+for ind = 1:length(stim)
+    stim(ind:ind+5,ind:ind+5) = 1;
+end
+stim = stim(1:npoints,1:npoints);
 %% Attention field
 attfield = exp(-1/2*((x-attx0)/attsd).^2);
 attfield = attgain*attfield  + 1;
@@ -26,77 +33,42 @@ attfield = attgain*attfield  + 1;
 stimdrive = conv2(stim, RF, 'same');
 numerator = stimdrive .* (attfield * ones(1,npoints));
 suppdrive = conv2(numerator, RFsupp, 'same');
+
 %% population response
 popresp = numerator ./ (suppdrive + sigma);
-voxresp = conv2(popresp, voxrf, 'same');
-
-%% visualize
-fH = figure(1); clf;
-set(fH, 'Color', 'w');
-lw = 3;
-fs = 16;
-subplot(3,3,1); plot(x, RF, x, RFsupp, 'LineWidth', lw);
-title('RF, center and surround');
-plotOptions(gca, fs);
-subplot(3,3,2); plot(x, attfield, 'LineWidth', lw);
-title('Attention field');
-plotOptions(gca, fs);
-subplot(3,3,3);
-imagesc(x, x, stim')
-title('Stimuli');
-plotOptions(gca, fs);
-subplot(3,3,4);
-imagesc(x, x, stimdrive);
-title('Stimulus drive');
-plotOptions(gca, fs);
-ylabel('Neural population')
-xlabel('Stimulus')
-subplot(3,3,5);
-imagesc(x, x, suppdrive);
-title('Suppressive drive');
-plotOptions(gca, fs);
-ylabel('Neural population')
-xlabel('Stimulus')
-subplot(3,3,6);
-imagesc(x, x, popresp);
-title('Population response');
-plotOptions(gca, fs);
-ylabel('Neural population')
-xlabel('Stimulus')
-subplot(3,3,7);
-locs  = -5:5;
-stimidx = zeros(size(locs));
-for ii = 1:length(locs)
-    k = locs(ii);
-    [~, stimidx(ii)] = min(abs(x-(attx0+k)));
+summation = conv2(popresp, RFsumm, 'same');
+if visualize
+    fH = figure; clf;
+    lw = 3;
+    fs = 10;
+    subplot(2,3,1); plot(x, RF, x, RFsupp, 'LineWidth', lw);
+    title('RF, center and surround');
+    plotOptions(gca, fs);
+    subplot(2,3,2); plot(x, attfield, 'LineWidth', lw);
+    title('Attention field');
+    plotOptions(gca, fs);
+    subplot(2,3,3);
+    imagesc(x, x, stim')
+    title('Stimuli');
+    plotOptions(gca, fs);
+    subplot(2,3,4);
+    imagesc(x, x, stimdrive);
+    title('Stimulus drive');
+    plotOptions(gca, fs);
+    ylabel('Neural population')
+    xlabel('Stimulus')
+    subplot(2,3,5);
+    imagesc(x, x, suppdrive);
+    title('Suppressive drive');
+    plotOptions(gca, fs);
+    ylabel('Neural population')
+    xlabel('Stimulus')
+    subplot(2,3,6);
+    imagesc(x, x, popresp);
+    title('Population response');
+    plotOptions(gca, fs);
+    ylabel('Neural population')
+    xlabel('Stimulus')
+    set(gcf,'Position',[0 0 600 350])
 end
-plot(x, popresp(:, stimidx), 'LineWidth', lw);
-ylim([0,1])
-title('Population response');
-set(gca, 'XTick', x(stimidx),  'XGrid', 'on', 'GridAlpha', 1)
-plotOptions(gca, fs);
-hold on
-[~, respidx] = max(popresp(:, stimidx)); yl = get(gca, 'YLim');
-plot(x(respidx)*[1 1], yl, 'r--')
-subplot(3,3,8);
-plot(x, popresp(stimidx,:), 'LineWidth', lw)
-ylim([0,1])
-title('Neural responses');
-plotOptions(gca, fs);
-hold on; set(gca, 'XTick', x(stimidx),  'XGrid', 'on', 'GridAlpha', 1)
-[~, respidx] = max(popresp(stimidx, :),[],2); yl = get(gca, 'YLim');
-plot(x(respidx)*[1 1], yl, 'r--')
-subplot(3,3,9);
-plot(x, voxresp(stimidx,:), 'LineWidth', lw)
-ylim([0,1])
-title('Voxel responses');
-plotOptions(gca, fs);
-hold on; set(gca, 'XTick', x(stimidx),  'XGrid', 'on', 'GridAlpha', 1)
-[~, respidx] = max(voxresp(stimidx, :),[],2); yl = get(gca, 'YLim');
-plot(x(respidx)*[1 1], yl, 'r--')
-set(gcf,'Position',[0 0 1600 1600])
-
-
-function plotOptions(ax, fs)
-set(ax, 'FontSize', fs)
 end
