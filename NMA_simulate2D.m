6c7690026f45810917d786769276269a2eb3955b
ekintuncok <tuncokekin@gmail.com>
2022-03-10 15:09:10 -0500

    clean the spatget rid of the spatial pooling

--- NMA_simulate2D.m
+++ NMA_simulate2D.m
@@ -1,4 +1,4 @@
-function [X, Y, stim, sptPopResp, pooledPopResp, predneuralweights, predsummedweights] = NMA_simulate2D(maindir, params)
+function [X, Y, stim, sptPopResp, pooledPopResp, predneuralweights] = NMA_simulate2D(maindir, params)
 
 mxecc       = params(1);
 RFsd        = params(2);
@@ -6,16 +6,16 @@ attgain     = params(3);
 attx0       = params(4);
 atty0       = params(5);
 attsd       = params(6);
-pooledPopRespsd = params(7);
-sigmaNorm       = params(8);
+sigmaNorm   = params(7);
+visualize   = params(8);
 
 %% Space (1D)
 step_size = mxecc/50;
 [X,Y] = meshgrid(-mxecc:step_size:mxecc);
 
 %% Stimuli
-load([fullfile(maindir, 'stimfiles/') 'stim.mat'])
-stim    = stim(:,:,1:end-1);
+stimtemp = load([fullfile(maindir, 'stimfiles/') 'stim.mat']);
+stim    = stimtemp.stim(:,:,1:end-1);
 stim    = logical(stim);
 
 inputStim = zeros(size(X,1),size(X,1),size(stim,3));
@@ -26,22 +26,27 @@ end
 %% Neural RFs
 RFsupp = exp(- ((X-0).^2 + (Y-0).^2)./(2*1.5*RFsd).^2);
 %% Voxel pooledPopResp (across neurons)
-RFsumm = exp(-((X-0).^2 +(Y-0).^2)./(2*pooledPopRespsd).^2); %%% keep in mmind
+% RFsumm = exp(-((X-0).^2 +(Y-0).^2)./(2*pooledPopRespsd).^2); %%% keep in mmind
 nCenters    = size(inputStim,1);
-x           = linspace(-8.8,8.8,nCenters); 
+x           = linspace(-8.8,8.8,nCenters);
 y           = linspace(-8.8,8.8,nCenters);
-nPRFs       = size(combvec(x,y),2);
-numSigmas   = 10;
-sigma       = linspace(0.1,5,numSigmas); % in degrees of visual field
-maxEccen    = sqrt(x(1).^2 + y(1).^2);
-sampleEccen = linspace(0,maxEccen,numSigmas);
+
+try
+    nPRFs       = size(combvec(x,y),2);
+    stimdrivenRFs(1:2,:) = combvec(x,y);
+catch
+    nPRFs       = size(CombVec(x,y),2);
+    stimdrivenRFs = CombVec(x,y);
+end
+% numSigmas   = 10;
+% sigma       = linspace(0.1,5,numSigmas); % in degrees of visual field
+% maxEccen    = sqrt(x(1).^2 + y(1).^2);
+% sampleEccen = linspace(0,maxEccen,numSigmas);
 
 % If we keep the x- and y- axes range (look a line up) at [-10 to 10] our
 % max eccentricity goes up to 14 degrees. Now we need to assign our sigma
-% value accordingly. 
+% value accordingly.
 
-stimdrivenRFs   = zeros(3,size(combvec(x,y),2));
-stimdrivenRFs(1:2,:) = combvec(x,y);
 
 for pRFInd = 1:nPRFs
     currCenter = stimdrivenRFs(1:2,pRFInd);
@@ -52,11 +57,11 @@ for pRFInd = 1:nPRFs
     stimdrivenRFs(3,pRFInd) = 0.05 + 0.1*eccen;
     continue
     %for eccenInd = 1:length(sampleEccen)
-        %thisEccen = eccen <= sampleEccen(eccenInd);
-        %if thisEccen == 1
-            %stimdrivenRFs(3,pRFInd) = sigma(eccenInd);
-            %break
-        %end
+    %thisEccen = eccen <= sampleEccen(eccenInd);
+    %if thisEccen == 1
+    %stimdrivenRFs(3,pRFInd) = sigma(eccenInd);
+    %break
+    %end
     %end
 end
 
@@ -70,10 +75,10 @@ numerator = zeros(size(stimdrivenRFs,2),size(inputStim,3));
 
 for rfind = 1:size(stimdrivenRFs,2)
     for ii = 1:size(inputStim,3)
-        % get the stim driven RF 
+        % get the stim driven RF
         RF = exp(-((X-(stimdrivenRFs(1,rfind))).^2 + ...
             (Y-(stimdrivenRFs(2,rfind))).^2)./(2*(stimdrivenRFs(3,rfind))).^2);
-         RF = RF./sum(RF(:));
+        RF = RF./sum(RF(:));
         % impose the attention field
         RFattn = RF .* attfield;
         % get the stimulus vectorized
@@ -90,21 +95,21 @@ for s = 1:size(stimdrive,2)
     stimdrive_pop(:,:,s) = reshape(stimdrive(:,s), [size(inputStim,1) size(inputStim,2)]);
     numerator_pop(:,:,s) = reshape(numerator(:,s), [size(inputStim,1) size(inputStim,2)]);
 end
-% 
+%
 % close all
 % figure
 % for s = 1:size(stimdrive,2)
 %     imagesc(stimdrive_pop(:,:,s))
 %     pause(1)
 % end
-% 
+%
 % close all
 % figure
 % for s = 1:size(stimdrive,2)
 %     imagesc(numerator_pop(:,:,s))
 %     pause(1)
 % end
-% 
+%
 
 % close all
 % iter =1;
@@ -129,12 +134,13 @@ suppdrive = convn(numerator_pop, RFsupp, 'same');
 
 %% population response
 sptPopResp = numerator_pop ./ (suppdrive + sigmaNorm);
-close all, for ii = 1:size(sptPopResp,3), imagesc(sptPopResp(:,:,ii)), pause(0.5), end
+if visualize
+    close all, for ii = 1:size(sptPopResp,3), imagesc(sptPopResp(:,:,ii)), pause(0.5), end
+end
 
-pooledPopResp = convn(sptPopResp, RFsumm, 'same');
+% pooledPopResp = convn(sptPopResp, RFsumm, 'same');
 % Go across time for a specific location in the pop response,let's say x =
-% 1, y = 2. 
+% 1, y = 2.
 predneuralweights = reshape(sptPopResp,[size(sptPopResp,1)*size(sptPopResp,2) size(sptPopResp,3)]);
-predsummedweights = reshape(pooledPopResp,[size(pooledPopResp,1)*size(pooledPopResp,2) size(pooledPopResp,3)]);
-
+% predsummedweights = reshape(pooledPopResp,[size(pooledPopResp,1)*size(pooledPopResp,2) size(pooledPopResp,3)]);
 end
