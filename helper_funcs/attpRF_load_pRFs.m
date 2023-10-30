f7e8b94641d55a6a02493892fcf9ba83c8b920b0
Ekin Tuncok <tuncokekin@gmail.com>
2023-10-30 13:51:35 -0400

    added the cartesian position option

--- helper_funcs/attpRF_load_pRFs.m
+++ helper_funcs/attpRF_load_pRFs.m
@@ -1,10 +1,24 @@
-function [eccen, angle] = attpRF_load_pRFs(prfFolder)
+function [cord1, cord2] = attpRF_load_pRFs(prfFolder, type)
+if strcmp(type, 'cart')
+    x_lh = MRIread(fullfile(prfFolder,'lh.x.mgz'));
+    x_rh = MRIread(fullfile(prfFolder, 'rh.x.mgz'));
+    x_coord = [x_lh.vol'; x_rh.vol'];
 
-eccen_lh = MRIread(fullfile(prfFolder,'lh.eccen.mgz'));
-eccen_rh = MRIread(fullfile(prfFolder, 'rh.eccen.mgz'));
-eccen = [eccen_lh.vol'; eccen_rh.vol'];
+    y_lh = MRIread(fullfile(prfFolder,'lh.y.mgz'));
+    y_rh = MRIread(fullfile(prfFolder, 'rh.y.mgz'));
+    y_coord = -1*[y_lh.vol'; y_rh.vol'];
+    cord1 = x_coord;
+    cord2 = y_coord;
 
-angle_lh = MRIread(fullfile(prfFolder,'lh.angle.mgz'));
-angle_rh = MRIread(fullfile(prfFolder, 'rh.angle.mgz'));
-angle_rad = [angle_lh.vol'; angle_rh.vol'];
-angle = rad2deg(angle_rad);
+elseif strcmp(type, 'polar')
+    eccen_lh = MRIread(fullfile(prfFolder,'lh.eccen.mgz'));
+    eccen_rh = MRIread(fullfile(prfFolder, 'rh.eccen.mgz'));
+    eccen = [eccen_lh.vol'; eccen_rh.vol'];
+
+    angle_lh = MRIread(fullfile(prfFolder,'lh.angle.mgz'));
+    angle_rh = MRIread(fullfile(prfFolder, 'rh.angle.mgz'));
+    angle_rad = [angle_lh.vol'; angle_rh.vol'];
+    angle = rad2deg(angle_rad);
+    cord1 =eccen;
+    cord2 =angle;
+end
