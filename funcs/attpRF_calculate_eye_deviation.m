ce17c21f916b6d5dd29a7b3e746e3ed968eea059
Ekin Tuncok <tuncokekin@gmail.com>
2023-10-30 15:00:00 -0400

    changed the script location

--- funcs/attpRF_calculate_eye_deviation.m
+++
@@ -1,6 +0,0 @@
-function [proportion_deviated] = attpRF_calculate_eye_deviation(data)
-
-distance_from_center = sqrt((data(:, 1)).^2 + (data(:, 2)).^2);
-proportion_deviated = sum(distance_from_center > 2)/length(distance_from_center);
-
-end
