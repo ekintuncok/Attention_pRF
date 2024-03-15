12305a08eb00afc91bafc1d97c98fe255366b6dc
Ekin Tuncok <tuncokekin@gmail.com>
2024-03-15 14:28:51 -0400

    folder name change

--- helper_scripts/calculate_bootstrapped_confidence_interval.m
+++ helper_scripts/calculate_bootstrapped_confidence_interval.m
@@ -15,7 +15,7 @@ ci_all = zeros(2, size(data, 2));
 
 for cond = 1:size(data, 2)
     boot_data = bootstrp(num_samples, fnc, data(:, cond));
-    ci_all(:, cond) = prctile(boot_shift_data, [low_prct_range, high_prct_range]);
+    ci_all(:, cond) = prctile(boot_data, [low_prct_range, high_prct_range]);
 end
 
 lower_ci = ci_all(1,:);
