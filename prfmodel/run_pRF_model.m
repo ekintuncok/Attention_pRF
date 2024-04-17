8358ba5347f526c26e7e173a892190b268d3f9a1
Ekin Tuncok <tuncokekin@gmail.com>
2024-04-17 13:29:45 -0400

    added the shuffled glm option to fit prf

--- prfmodel/run_pRF_model.m
+++ prfmodel/run_pRF_model.m
@@ -1,19 +1,11 @@
-function results = run_pRF_model(subject_info, condition, glm_folder)
+function results = run_pRF_model(subject_info, glm_folder)
 
 % This function runs pRF model on attention pRF subject data using prfVistasoft
-% wrapper, and VistaLab software. 
+% wrapper, and VistaLab software.
 % Example calls:
-
-% results = run_pRF_model('wlsubj123', 1, 'avg_betas')
-% results = run_pRF_model('wlsubj123', 1, 'main')
-% results = run_pRF_model('wlsubj123', 2, 'main')
-% results = run_pRF_model('wlsubj123', 3, 'main') ...
-% the condition refers to which attentional cueing produced the
-% corresponding beta coefficients, goes from 1 to 5 for attend up, down,
-% left, right and distributed. All this data reside in the main glmdenoise
-% folder. The avg_betas folder has the averaged beta coefficients across
-% attention conditions. There is only one condition option for this folder
-% (1). 
+% results = run_pRF_model('wlsubj123', 'avg_betas')
+% results = run_pRF_model('wlsubj123', 'main')
+% results = run_pRF_model('wlsubj123', 'shuffled')
 
 addpath(genpath('/scratch/et2160/toolboxes/vistasoft'));
 addpath(genpath('/scratch/et2160/toolboxes/prfVista'));
@@ -27,28 +19,46 @@ stimradius = 12;
 % force the condition to be equal to one if average betas are inputted to
 % the pRF model:
 if strcmp(glm_folder, 'avg_betas')
-    condition = 1;
+    numCond = 1;
+    cond_folder = {'avg'};
+    % the condition refers to which attentional cueing produced the
+    % corresponding beta coefficients, goes from 1 to 5 for attend up, down,
+    % left, right and distributed. All this data reside in the main glmdenoise
+    % folder. The avg_betas folder has the averaged beta coefficients across
+    % attention conditions. There is only one condition option for this folder
+    % (1).
+else
+    numCond = 1:5;
+    cond_folder = {'1','2','3','4','5'};
+end
+
+if strcmp(glm_folder, 'main') || strcmp(glm_folder, 'avg_betas')
+    prf_folder = 'prfFolder_2';
+elseif strcmp(glm_folder, 'shuffled')
+    prf_folder = 'shuffled';
 end
 
 glm_results_dir = fullfile(path2project, 'derivatives', 'GLMdenoise', glm_folder);
 prf_results_dir = fullfile(path2project, 'derivatives', 'prfs');
-for h = 1:length(hemispheres)
-    fprintf('Running subject = %s, hemisphere = %s\n',subject_info, hemispheres{h});
+for c = 1:length(numCond)
+    for h = 1:length(hemispheres)
+        fprintf('>>>> Currently running the %s model on subject: %s condition: %s hemisphere: %s\n', glm_folder, subject, cond_folder{c}, hemispheres{h}); 
 
-    data = fullfile(glm_results_dir, sprintf('sub-%s/ses-nyu3t99/niftifiles/%s.modelmd.%i.nii.gz', subject, hemispheres{h}, condition));
+        data = fullfile(glm_results_dir, sprintf('sub-%s/ses-nyu3t99/niftifiles/%s.modelmd.%i.nii.gz', subject, hemispheres{h}, numCond(c)));
 
-    % we will set some default steps to 0 here. This is because instead of
-    % fMRI time series, we are using the beta coefficients representing the
-    % mapping stimulus location from GLM. We will set detrending,
-    % decimating and percent change calculation to 0, we will also change
-    % the hrf to none (or impulse function): 
-    results = prfVistasoft(stim, data, stimradius,...
-        'tr', 1, 'detrend', 0, 'hrfparams', 'none', 'decimate',0, 'calcPC', false);
+        % we will set some default steps to 0 here. This is because instead of
+        % fMRI time series, we are using the beta coefficients representing the
+        % mapping stimulus location from GLM. We will set detrending,
+        % decimating and percent change calculation to 0, we will also change
+        % the hrf to none (or impulse function):
+        results = prfVistasoft(stim, data, stimradius,...
+            'tr', 1, 'detrend', 0, 'hrfparams', 'none', 'decimate',0, 'calcPC', false);
 
-    save_folder = fullfile(prf_results_dir, sprintf('sub-%s/ses-nyu3t99/prfFolder_2/avg/', subject));
-    if ~exist(save_folder, 'dir')
-        mkdir(save_folder);
+        save_folder = fullfile(prf_results_dir, sprintf('sub-%s/ses-nyu3t99/%s/%s/', subject, prf_folder, cond_folder{c}));
+        if ~exist(save_folder, 'dir')
+            mkdir(save_folder);
+        end
+        save(fullfile(save_folder, sprintf('%s.prfFit.wholeBrain.mat', hemispheres{h})), '-v7.3')
     end
-    save(fullfile(save_folder, sprintf('%s.prfFit.wholeBrain.mat', hemispheres{h})), '-v7.3')
 end
 
