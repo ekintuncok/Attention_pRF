0d9f7ab2700b8d122c8c7dba240f7d5491d32c93
Ekin Tuncok <tuncokekin@gmail.com>
2024-06-04 17:45:51 -0400

    final s0

--- s0_attentionpRF.m
+++ s0_attentionpRF.m
@@ -6,22 +6,22 @@ scriptDir = fileparts(mfilename('fullpath'));
 cd(scriptDir)
 addpath(genpath(fullfile(scriptDir)));
 
-aim = input('Please enter if you want to just visualize (press (0)) or analyze from scratch (this asks for dependencies, press (1)): ');
+toolboxPath = input('Please enter the path that contains all dependencies, enter 0 if you are only visualizing the processed data: ', 's');
 
-if aim == 1
-    toolboxPath = input('Please enter the path that contains all dependencies: ', 's');
+if ~strcmp(toolboxPath, '0')
     if ~exist(toolboxPath, 'dir')
         warning('Inputted directory does not exist, double check if it is accurate.')
+    else
+        %%% ---------------- DEPENDENCIES ---------------- %%%
+        %setenv('SUBJECTS_DIR','/Volumes/server/Projects/attentionpRF/derivatives/freesurfer/')
+        addpath(genpath(fullfile(toolboxPath, 'GLMDenoise')));
+        addpath(genpath(fullfile(toolboxPath, 'vistasoft')));
+        addpath(genpath(fullfile(toolboxPath, 'mri_tools')));
+        addpath(genpath(fullfile(toolboxPath, 'prfVista')));
+        addpath(genpath(fullfile(toolboxPath, 'cvncode')));
+        addpath(genpath(fullfile(toolboxPath, 'knkUtils')));
+        %%% ---------------- DEPENDENCIES ---------------- %%%
     end
-    %%% ---------------- DEPENDENCIES ---------------- %%%
-    %setenv('SUBJECTS_DIR','/Volumes/server/Projects/attentionpRF/derivatives/freesurfer/')
-    addpath(genpath(fullfile(toolboxPath, 'GLMDenoise')));
-    addpath(genpath(fullfile(toolboxPath, 'vistasoft')));
-    addpath(genpath(fullfile(toolboxPath, 'mri_tools')));
-    addpath(genpath(fullfile(toolboxPath, 'prfVista')));
-    addpath(genpath(fullfile(toolboxPath, 'cvncode')));
-    addpath(genpath(fullfile(toolboxPath, 'knkUtils')));
-    %%% ---------------- DEPENDENCIES ---------------- %%%
     if ~exist(fullfile(toolboxPath, 'GLMDenoise'), 'dir') || ...
             ~exist(fullfile(toolboxPath, 'vistasoft'), 'dir') || ...
             ~exist(fullfile(toolboxPath, 'mri_tools'), 'dir') || ...
@@ -48,23 +48,11 @@ session           = 'nyu3t99';
 sessionList       = {'nyu3t01','nyu3t02','nyu3t03','nyu3t04'};
 dataFolder        = 'fmriprep';
 task              = 'attprf';
-
 designFolder      = 'main'; % this could be 'shuffled' for the control analysis of pRF center shifts.
 ROIs = {'V1','V2','V3', 'hV4','V3AB', 'LO1'};
 
-num_iterations = 1000;
-bar_loc = 1:48;
-attend_up    = 1;
-attend_down  = 2;
-attend_left  = 3;
-attend_right = 4;
-attend_neutral = 5;
-num_targets  = 4;
-mappingStimLeft = [5:9, 35:38];
-mappingStimRight = [17:21, 35:38];
-mappingStimUpper = [11:14, 29:33];
-mappingStimLower = [11:14, 41:45];
-MS_overlappng_locs = cat(1, mappingStimUpper, mappingStimLower, mappingStimLeft, mappingStimRight);
+% Save the configuration
+save('config.mat', 'toolboxPath', 'path2project', 'subject_list','sessionList','ROIs');
 
 %s1_attprf_prepareforGLM(path2project, subject, task, sessionList)
 %s2_attprf_prepareforpRFfitting(path2project, subject, session, designFolder)
