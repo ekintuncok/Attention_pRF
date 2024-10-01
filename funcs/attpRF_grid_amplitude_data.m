ccb7803eb3005e4731c30b292f305b8bd0857a61
Ekin Tuncok <tuncokekin@gmail.com>
2024-10-01 11:39:49 -0400

    revision

--- funcs/attpRF_grid_amplitude_data.m
+++ funcs/attpRF_grid_amplitude_data.m
@@ -1,4 +1,4 @@
-function visual_field_map = attpRF_grid_amplitude_data(currROI, path2project, subject, session, designFolder, x_grid, y_grid, masked_grid, rotation)
+function visual_field_map = attpRF_grid_amplitude_data(currROI, path2project, subject, session, designFolder, x_grid, y_grid, masked_grid, rotation, which_betas)
 
 attend_up = 1;
 attend_down = 2;
@@ -16,17 +16,21 @@ else
 end
 
 % load the GLM results:
-GLMfolder         = sprintf('%sderivatives/GLMdenoise/%s/%s/ses-%s/', path2project, designFolder, subject, session);
+GLMfolder         = sprintf('%s/derivatives/GLMdenoise/%s/%s/ses-%s/', path2project, designFolder, subject, session);
 prfFolder = fullfile(path2project, 'derivatives', 'prfs', sprintf('%s',subject),...
-    sprintf('ses-%s',session), 'prfFolder_2', 'avg/');
+    sprintf('ses-%s',session), 'main', 'avg/');
 
 % load the pRF results:
 load([GLMfolder sprintf('%s_ses-%s_%s_results.mat', subject, session, designFolder)]);% this loads 'betas' and 'R2'
 
-for idx = 1:length(betas)-2
-    thresholded_betas(:,:,idx) = squeeze(betas{idx});
+if strcmp(which_betas, 'all')
+    for idx = 1:length(betas)-2
+        thresholded_betas(:,:,idx) = squeeze(betas{idx});
+    end
+    thresholded_betas(:,2,:) = squeeze(betas{6}); % null trial estimates
+elseif strcmp(which_betas, 'null')
+    thresholded_betas = squeeze(betas{6}); % null trial estimates
 end
-thresholded_betas(:,2,:) = squeeze(betas{6}); % null trial estimates
 
 R2 = R2';
 indices = currROI ~= 0 & R2 > 3;
@@ -38,12 +42,18 @@ for conds = 1:length(conditions)
     shifted_pangle = pangle_to_shift - target_angle_shift_deg(conds);
     shifted_pangle_radians = deg2rad(shifted_pangle);
     [shifted_X, shifted_Y] = pol2cart(shifted_pangle_radians, eccen);
-    betas = mean(thresholded_betas(indices,:,conds),2)-mean(thresholded_betas(indices,:,5),2);
-    %betas = thresholded_betas(indices,conds)-thresholded_betas(indices,5);
-
+    if strcmp(which_betas, 'all')
+        betas = mean(thresholded_betas(indices,:,conds),2)-mean(thresholded_betas(indices,:,5),2);
+    elseif  strcmp(which_betas, 'null')
+        betas = mean(thresholded_betas(indices,conds),2)-mean(thresholded_betas(indices,5),2);
+    end
     betas = double(betas);
     interpolated_visual_field_map = griddata(shifted_X(indices), shifted_Y(indices), betas, x_grid, y_grid);
     interpolated_visual_field_map = interpolated_visual_field_map.*masked_grid;
     interpolated_visual_field_map(isnan(interpolated_visual_field_map)) = 0;
     visual_field_map(:,:,conds) = interpolated_visual_field_map;
 end
+
+
+
+
--- s3_attpRF_visualizeprfsolutions.m
+++ s3_attpRF_visualizeprfsolutions.m
@@ -1,14 +1,12 @@
 s0_attentionpRF;
-conditions = {'1','2','3','4','5'};
+%conditions = {'1','2','3','4','5'};
+conditions = {'avg'};
+
 folderTag = 'main'; % or 'shuffled'
 
 for subid = 1:length(subject_list)
     subjecttm = subject_list(subid).name;
     subject = subjecttm(5:end);
-    figureDir         = fullfile(path2project, 'derivatives','figures',sprintf('sub-%s',subject));
-    if ~exist(figureDir, 'dir')
-        mkdir(figureDir);
-    end
 
     % convert pRFs to .mgz
     convermgz = true;
@@ -19,21 +17,3 @@ for subid = 1:length(subject_list)
         end
     end
 end
-%
-% for c = 1:length(conditions)
-%     prfFolder         = fullfile(path2project, 'derivatives', 'prfs', sprintf('/sub-%s/ses-%s/%s/%s/', subject, session, folderTag, conditions{c}));
-%     figuresFolder     = fullfile(path2project, 'derivatives', 'prfs', sprintf('/sub-%s/ses-%s/Figures/%s', subject, session, conditions{c}));
-%     if ~exist(figuresFolder, 'dir')
-%         mkdir(figuresFolder);
-%     end
-%     getpRFparameterdist(path2project, subject, prfFolder, figuresFolder)
-% end
-
-
-% for subid = 1:length(subject_list)
-%     subjecttm = subject_list(subid).name;
-%     subject = subjecttm(5:end);
-%     disp(subject)
-%     which_roi_boundaries = 'hand';
-%     getFlatMaps(path2project,subject,session,conditions, which_roi_boundaries, folderTag)
-% end
